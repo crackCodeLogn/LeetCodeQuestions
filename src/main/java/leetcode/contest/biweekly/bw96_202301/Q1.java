3e98e82fa27f41a41d4a302ee51f45f341489b8f
Vivek <vivektheintel@gmail.com>
2023-01-21 09:18:20 -0500

    rearranging packages for contest

---
+++ src/main/java/leetcode/contest/biweekly/bw96_202301/Q1.java
@@ -0,0 +1,8 @@
+package leetcode.contest.biweekly.bw96_202301;
+
+/**
+ * @author Vivek
+ * @since 2023-01-21
+ */
+public class Q1 {
+}
---
+++ src/main/java/leetcode/contest/weekly/w329_202301/Q1.java
@@ -0,0 +1,8 @@
+package leetcode.contest.weekly.w329_202301;
+
+/**
+ * @author Vivek
+ * @since 2023-01-21
+ */
+public class Q1 {
+}
