6d281d877adcd5fdd288429f7bc00b6bea0b11c6
Vivek <vivektheintel@gmail.com>
2023-04-01 23:13:24 -0400

    w339: 2xAC: q6362, q6363

---
+++ src/main/java/leetcode/contest/weekly/w339_202401/Q6362_E_FindTheLongestBalancedSubstringOfABinaryString.java
@@ -0,0 +1,48 @@
+package leetcode.contest.weekly.w339_202401;
+
+/**
+ * @author Vivek
+ * @since 2023-04-02
+ */
+public class Q6362_E_FindTheLongestBalancedSubstringOfABinaryString {
+
+    public static void main(String[] args) {
+        Q6362_E_FindTheLongestBalancedSubstringOfABinaryString q6362EFindTheLongestBalancedSubstringOfABinaryString = new Q6362_E_FindTheLongestBalancedSubstringOfABinaryString();
+        System.out.println(q6362EFindTheLongestBalancedSubstringOfABinaryString.findTheLongestBalancedSubstring("01000111"));
+        System.out.println(q6362EFindTheLongestBalancedSubstringOfABinaryString.findTheLongestBalancedSubstring("00111"));
+        System.out.println(q6362EFindTheLongestBalancedSubstringOfABinaryString.findTheLongestBalancedSubstring("111"));
+        System.out.println(q6362EFindTheLongestBalancedSubstringOfABinaryString.findTheLongestBalancedSubstring("10011"));
+        System.out.println(q6362EFindTheLongestBalancedSubstringOfABinaryString.findTheLongestBalancedSubstring("11110"));
+    }
+
+    public int findTheLongestBalancedSubstring(String s) {
+        int c0 = 0, c1 = 0, n = s.length();
+        if (n == 1) return 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') c1++;
+            else c0++;
+        }
+        if (c0 == 0 || c1 == 0) return 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            c1 = 0;
+            while (i < n && s.charAt(i) == '1') i++;
+            if (i >= n) break;
+            c0 = 1;
+            boolean flag = false;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(j) == '0' && !flag) c0++;
+                else if (s.charAt(j) == '0' && flag) {
+                    i = j - 1;
+                    break;
+                } else {
+                    flag = true;
+                    c1++;
+                    max = Math.max(max, 2 * Math.min(c0, c1));
+                }
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/leetcode/contest/weekly/w339_202401/Q6363_M_ConvertArrayInto2dArrayWithConditions.java
@@ -0,0 +1,52 @@
+package leetcode.contest.weekly.w339_202401;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Vivek
+ * @since 2023-04-02
+ */
+public class Q6363_M_ConvertArrayInto2dArrayWithConditions {
+
+    private static <K> void augmentCount(Map<K, Integer> mapper, K key) {
+        Integer val = mapper.get(key);
+        if (val == null) val = 1;
+        else val++;
+        mapper.put(key, val);
+    }
+
+    public static void main(String[] args) {
+        Q6363_M_ConvertArrayInto2dArrayWithConditions q6363MConvertArrayInto2DArrayWithConditions = new Q6363_M_ConvertArrayInto2dArrayWithConditions();
+        System.out.println(q6363MConvertArrayInto2DArrayWithConditions.findMatrix(new int[]{
+                1, 3, 4, 1, 2, 3, 1
+        }));
+        System.out.println(q6363MConvertArrayInto2DArrayWithConditions.findMatrix(new int[]{
+                1, 2, 3, 4
+        }));
+    }
+
+    public List<List<Integer>> findMatrix(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Set<Integer>> intermed = new ArrayList<>();
+
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) augmentCount(count, num);
+        int maxV = 0;
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            Integer v = entry.getValue();
+            if (v > maxV) maxV = v;
+        }
+        while (maxV-- > 0) intermed.add(new HashSet<>());
+        count.forEach((k, v) -> {
+            int i = 0;
+            while (v-- > 0) intermed.get(i++).add(k);
+        });
+        intermed.forEach(set -> result.add(new ArrayList<>(set)));
+        return result;
+    }
+}
