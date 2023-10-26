ba94588a78467f99f91d355b34205651004862b3
Vivek <vivektheintel@gmail.com>
2023-10-26 11:55:21 -0400

    prep,1,medium,3,20231026

---
+++ src/main/java/leetcode/practise/Q0003_M_LongestSusbtringWithoutRepeatingCharacters.java
@@ -0,0 +1,41 @@
+package leetcode.practise;
+
+import java.util.Arrays;
+
+/**
+ * @author Vivek
+ * @since 2023-10-26
+ */
+public class Q0003_M_LongestSusbtringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        Q0003_M_LongestSusbtringWithoutRepeatingCharacters longestSusbtringWithoutRepeatingCharacters = new Q0003_M_LongestSusbtringWithoutRepeatingCharacters();
+        //System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcabcbb"));
+        //System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("bbbb"));
+        //System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("cbabzgpb"));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.isEmpty()) return 0;
+        int[] counter = new int[130];
+        int length = 0, maxLength = -1;
+        Arrays.fill(counter, -1);
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (counter[ch] != -1) {
+                int target = counter[ch];
+                for (int j = 0; j < counter.length; j++) {
+                    if (counter[j] != -1 && counter[j] <= target) {
+                        counter[j] = -1;
+                        length--;
+                    }
+                }
+            }
+            length++;
+            maxLength = Math.max(maxLength, length);
+            counter[ch] = i;
+        }
+        return maxLength;
+    }
+}
