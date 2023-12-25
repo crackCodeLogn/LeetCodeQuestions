affd79b36d27b96cd894421e3c02ae30a6f91e62
Vivek <vivektheintel@gmail.com>
2023-12-25 15:29:10 -0500

    interview practise - q383 - e

---
+++ src/main/java/leetcode/practise/blind75/Q0383_E_RansomNote.java
@@ -0,0 +1,18 @@
+package leetcode.practise.blind75;
+
+/**
+ * @author Vivek
+ * @since 2023-12-26
+ */
+public class Q0383_E_RansomNote {
+    public boolean canConstruct(String data, String magazine) {
+        int[] cnt = new int[130];
+        for (int i = 0; i < magazine.length(); i++) cnt[magazine.charAt(i)]++;
+        for (int i = 0; i < data.length(); i++) {
+            int ch = data.charAt(i);
+            if (cnt[ch] <= 0) return false;
+            cnt[ch]--;
+        }
+        return true;
+    }
+}
