4dcf1c53b3293f650120a39e85cc5c69b7cd9983
Vivek <vivektheintel@gmail.com>
2023-12-25 13:52:37 -0500

    interview practise - q226 - e

---
+++ src/main/java/leetcode/practise/blind75/Q0226_E_InvertBinaryTree.java
@@ -0,0 +1,24 @@
+package leetcode.practise.blind75;
+
+/**
+ * @author Vivek
+ * @since 2023-12-26
+ */
+public class Q0226_E_InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = tmp;
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+}
