9cf93f0246c2f112cbb44c1e6913acb3b230d8e7
Vivek <vivektheintel@gmail.com>
2023-12-30 19:18:33 -0500

    daily practise - q1624 - e - update using matrix

--- src/main/java/leetcode/practise/Q1624_E_LargestSubstringBetweenTwoCharacters.java
+++ src/main/java/leetcode/practise/Q1624_E_LargestSubstringBetweenTwoCharacters.java
@@ -1,5 +1,7 @@
 package leetcode.practise;
 
+import java.util.Arrays;
+
 /**
  * @author Vivek
  * @since 2023-12-31
@@ -12,6 +14,22 @@ public class Q1624_E_LargestSubstringBetweenTwoCharacters {
     }
 
     public int maxLengthBetweenEqualCharacters(String s) {
+        int max = -1;
+        int[][] location = new int[130][2];
+        for (int i = 0; i < location.length; i++) Arrays.fill(location[i], -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (location[ch][0] == -1) location[ch][0] = i;
+            else {
+                location[ch][1] = i;
+                max = Math.max(max, location[ch][1] - location[ch][0] - 1);
+            }
+        }
+        return max;
+    }
+
+    public int maxLengthBetweenEqualCharacters2(String s) {
         int max = -1;
         for (int i = 0; i < s.length(); i++) {
             int i2 = s.lastIndexOf(s.charAt(i));
