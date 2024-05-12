f86854047e9f3c925e144d0544d355dbae2240b3
Vivek <vivektheintel@gmail.com>
2024-05-12 00:38:16 -0400

    timing improvements on q3148 - m

--- src/main/java/leetcode/contest/weekly/w397_202405/Q3148_M_MaximumDifferenceScoreInAGrid_WA_AC.java
+++ src/main/java/leetcode/contest/weekly/w397_202405/Q3148_M_MaximumDifferenceScoreInAGrid_WA_AC.java
@@ -30,7 +30,30 @@ public class Q3148_M_MaximumDifferenceScoreInAGrid_WA_AC {
         System.out.println(q3148MMaximumDifferenceScoreInAGridWAAC.maxScore(grid)); // 9
     }
 
-    public int maxScore(List<List<Integer>> grid) { // AC, but 8 minutes after contest
+    public int maxScore(List<List<Integer>> grid) { //29ms -> optimized after maxScore3
+        int max = Integer.MIN_VALUE, r = grid.size(), c = grid.get(0).size(), max2 = Integer.MIN_VALUE;
+        int[][] data = new int[r][c];
+        int[][] maximum = new int[r][c];
+        for (int i = 0; i < r; i++) for (int j = 0; j < c; j++) maximum[i][j] = Integer.MIN_VALUE;
+
+        for (int i = r - 1; i >= 0; i--) {
+            for (int j = c - 1; j >= 0; j--) {
+                int m = 0; // if no move
+                if (i < r - 1)
+                    maximum[i][j] = Math.max(maximum[i][j], grid.get(i + 1).get(j) - grid.get(i).get(j) + data[i + 1][j]);
+                if (j < c - 1)
+                    maximum[i][j] = Math.max(maximum[i][j], grid.get(i).get(j + 1) - grid.get(i).get(j) + data[i][j + 1]);
+
+                m = Math.max(m, maximum[i][j]);
+                max2 = Math.max(max2, maximum[i][j]);
+                data[i][j] = m;
+                max = Math.max(max, data[i][j]);
+            }
+        }
+        return max == 0 ? max2 : max;
+    }
+
+    public int maxScore3(List<List<Integer>> grid) { // AC, but 8 minutes after contest -> 56ms
         int max = Integer.MIN_VALUE, r = grid.size(), c = grid.get(0).size(), max2 = Integer.MIN_VALUE;
         int[][] data = new int[r][c];
         TreeSet<Integer>[][] sets = new TreeSet[r][c];
