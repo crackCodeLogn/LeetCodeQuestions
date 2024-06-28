0f3ad78ac1d04d89671360ae900bf38a4a642d1d
Vivek <vivektheintel@gmail.com>
2024-06-28 10:47:31 -0400

    daily question - q3 - m #retried #diff-approach

--- src/main/java/leetcode/practise/Q0003_M_LongestSusbtringWithoutRepeatingCharacters.java
+++ src/main/java/leetcode/practise/Q0003_M_LongestSusbtringWithoutRepeatingCharacters.java
@@ -1,6 +1,8 @@
 package leetcode.practise;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Vivek
@@ -10,13 +12,36 @@ public class Q0003_M_LongestSusbtringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
         Q0003_M_LongestSusbtringWithoutRepeatingCharacters longestSusbtringWithoutRepeatingCharacters = new Q0003_M_LongestSusbtringWithoutRepeatingCharacters();
-        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("bbbb"));
-        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("pwwkew"));
-        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("cbabzgpb"));
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcabcbb")); // 3
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("bbbb")); // 1
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("pwwkew")); // 3
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("cbabzgpb")); // 5
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("abcde")); // 5
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("abba")); // 2
+        System.out.println(longestSusbtringWithoutRepeatingCharacters.lengthOfLongestSubstring("tmmzuxt")); // 5
     }
 
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s) { // 6ms
+        if (s.isEmpty()) return 0;
+
+        Map<Character, Integer> data = new HashMap<>();
+        int len = 1, prev = 0;
+        int i;
+
+        for (i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!data.containsKey(ch) || prev > data.get(ch)) data.put(ch, i);
+            else {
+                len = Math.max(len, i - prev);
+                prev = Math.max(prev, data.get(ch)) + 1;
+                data.put(ch, i);
+            }
+        }
+        len = Math.max(len, i - prev);
+        return len;
+    }
+
+    public int lengthOfLongestSubstring2(String s) { // 4ms
         if (s.isEmpty()) return 0;
         int[] counter = new int[130];
         int length = 0, maxLength = -1;
