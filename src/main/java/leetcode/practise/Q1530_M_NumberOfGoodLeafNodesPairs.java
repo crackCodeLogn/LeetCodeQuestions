4325c2f6e972d641f8fe96c819f7a4ca3d7ba26a
Vivek <vivektheintel@gmail.com>
2024-07-21 22:56:44 -0400

    daily question - q1530 - m #interesting #tree #redo

---
+++ src/main/java/leetcode/practise/Q1530_M_NumberOfGoodLeafNodesPairs.java
@@ -0,0 +1,58 @@
+package leetcode.practise;
+
+/**
+ * @author Vivek
+ * @since 2024-07-22
+ */
+public class Q1530_M_NumberOfGoodLeafNodesPairs {
+
+    private int ans = 0;
+
+    public int countPairs(TreeNode root, int distance) {
+        dfs(root, distance);
+
+        return ans;
+    }
+
+    private int[] dfs(TreeNode root, int distance) {
+        int[] d = new int[distance + 1]; // {distance: the number of leaf nodes}
+        if (root == null)
+            return d;
+        if (root.left == null && root.right == null) {
+            d[0] = 1;
+            return d;
+        }
+
+        int[] dl = dfs(root.left, distance);
+        int[] dr = dfs(root.right, distance);
+
+        for (int i = 0; i < distance; ++i)
+            for (int j = 0; j < distance; ++j)
+                if (i + j + 2 <= distance)
+                    ans += dl[i] * dr[j];
+
+        for (int i = 0; i < distance; ++i)
+            d[i + 1] = dl[i] + dr[i];
+
+        return d;
+    }
+
+    private static final class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
