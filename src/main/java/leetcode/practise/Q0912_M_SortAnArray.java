430f02a059337ca993d80121e82003b1176da753
Vivek <vivektheintel@gmail.com>
2024-07-24 20:44:28 -0400

    daily question - q912 - m

---
+++ src/main/java/leetcode/practise/Q0912_M_SortAnArray.java
@@ -0,0 +1,14 @@
+package leetcode.practise;
+
+import java.util.Arrays;
+
+/**
+ * @author Vivek
+ * @since 2024-07-25
+ */
+public class Q0912_M_SortAnArray {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
