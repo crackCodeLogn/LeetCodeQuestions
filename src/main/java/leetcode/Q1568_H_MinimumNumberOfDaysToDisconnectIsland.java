29272d874131ae46d005419603c059fcd6597098
Vivek <vivektheintel@gmail.com>
2024-08-10 20:12:30 -0400

    daily question - q1568 - h #matrix #redo

---
+++ src/main/java/leetcode/Q1568_H_MinimumNumberOfDaysToDisconnectIsland.java
@@ -0,0 +1,62 @@
+package leetcode;
+
+/**
+ * @author Vivek
+ * @since 2024-08-11
+ */
+public class Q1568_H_MinimumNumberOfDaysToDisconnectIsland {
+
+    private final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public static void main(String[] args) {
+        Q1568_H_MinimumNumberOfDaysToDisconnectIsland q1568HMinimumNumberOfDaysToDisconnectIsland = new Q1568_H_MinimumNumberOfDaysToDisconnectIsland();
+        System.out.println(q1568HMinimumNumberOfDaysToDisconnectIsland.minDays(new int[][]{{0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}}));
+        System.out.println(q1568HMinimumNumberOfDaysToDisconnectIsland.minDays(new int[][]{{1, 1}}));
+    }
+
+    public int minDays(int[][] grid) {
+        if (disconnected(grid))
+            return 0;
+
+        // Try to remove 1 land.
+        for (int i = 0; i < grid.length; ++i)
+            for (int j = 0; j < grid[0].length; ++j)
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;
+                    if (disconnected(grid))
+                        return 1;
+                    grid[i][j] = 1;
+                }
+
+        // Remove 2 lands.
+        return 2;
+    }
+
+    private boolean disconnected(int[][] grid) {
+        int islandsCount = 0;
+        boolean[][] seen = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; ++i)
+            for (int j = 0; j < grid[0].length; ++j) {
+                if (grid[i][j] == 0 || seen[i][j])
+                    continue;
+                if (++islandsCount > 1)
+                    return true;
+                dfs(grid, i, j, seen);
+            }
+
+        return islandsCount != 1;
+    }
+
+    private void dfs(int[][] grid, int i, int j, boolean[][] seen) {
+        seen[i][j] = true;
+        for (int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (x < 0 || x == grid.length || y < 0 || y == grid[0].length)
+                continue;
+            if (grid[x][y] == 0 || seen[x][y])
+                continue;
+            dfs(grid, x, y, seen);
+        }
+    }
+}
