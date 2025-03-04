67421ce462a9a87126d6c276995dd393fc7a3b61
Vivek <vivektheintel@gmail.com>
2025-03-04 13:01:21 -0500

    daily question - q1780 - m #math

---
+++ src/main/java/leetcode/practise/Q1780_M_CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,44 @@
+package leetcode.practise;
+
+import java.util.TreeSet;
+
+/**
+ * @author Vivek
+ * @since 2025-03-04
+ */
+public class Q1780_M_CheckIfNumberIsASumOfPowersOfThree {
+  private static final int LIMIT = 10_000_000;
+  private static final TreeSet<Integer> THREE = new TreeSet<>();
+
+  public static void main(String[] args) {
+    Q1780_M_CheckIfNumberIsASumOfPowersOfThree q1780MCheckIfNumberIsASumOfPowersOfThree =
+        new Q1780_M_CheckIfNumberIsASumOfPowersOfThree();
+    System.out.println(q1780MCheckIfNumberIsASumOfPowersOfThree.checkPowersOfThree(3));
+    System.out.println(q1780MCheckIfNumberIsASumOfPowersOfThree.checkPowersOfThree(1));
+    System.out.println(q1780MCheckIfNumberIsASumOfPowersOfThree.checkPowersOfThree(2));
+    System.out.println(q1780MCheckIfNumberIsASumOfPowersOfThree.checkPowersOfThree(12));
+    System.out.println(q1780MCheckIfNumberIsASumOfPowersOfThree.checkPowersOfThree(91));
+    System.out.println(q1780MCheckIfNumberIsASumOfPowersOfThree.checkPowersOfThree(21));
+  }
+
+  public boolean checkPowersOfThree(int n) { // 1ms, beats 15.70%
+    if (THREE.isEmpty()) {
+      int v = 3;
+      THREE.add(1);
+      do {
+        THREE.add(v);
+        v *= 3;
+      } while (v >= 0 && v < LIMIT);
+    }
+
+    int last = 0;
+    while (n > 0) {
+      Integer v = THREE.floor(n);
+      if (last == v) return false;
+      last = v;
+      n -= v;
+      if (n == 0) return true;
+    }
+    return false;
+  }
+}
